6042d25731e0272fed607de9d7a6f99d90a0ed96
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-03-29 13:49:11 -0300

    salvando alterações

--- index.js
+++ index.js
@@ -12,23 +12,26 @@
 
 
 const express = require('express')
-const port = 3000
+const cors = require('cors')
+const port = 3001
 const app = express()
 app.use(express.json()) /* usar esse use sempre quando for utilizar body antigo */
+app.use(cors())
 const uuid = require('uuid')
+
 
 const users = []/*  nunca usar isso para armazenzar < */
 
 const checkUserId = (request, response, next) => {
-    
-     const {id} = request.params
 
-     const index = users.findIndex(user => user.id === id)
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
 
     if (index < 0) {
         return response.status(404).json({ error: "User not found" })
     }
-    
+
     request.userIndex = index
     request.userId = id
 
@@ -46,18 +49,18 @@ app.post('/users', (request, response) => {
     return response.status(201).json(user)
 })
 
-app.put('/users:id', checkUserId,(request, response) => {
-    
+app.put('/users:id', checkUserId, (request, response) => {
+
     const { name, age } = request.body
-    const index = request.userIndex 
+    const index = request.userIndex
     const id = request.userId
-    const updatedUser = {id, name, age}
+    const updatedUser = { id, name, age }
     users[index] = updatedUser
     return response.json(updatedUser)
 
 })
 
-app.delete('/users:id',checkUserId, (request, response) => {
+app.delete('/users:id', checkUserId, (request, response) => {
     const index = request.userIndex
     users.splice(index, 1)
     return response.status(204).json()
